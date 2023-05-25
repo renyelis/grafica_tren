d72e8b0f26a78542ba2e51d5983ab3f4a6ffdd7c
lisethcorrea <lisethcorrea@colegioguanenta.edu.com>
2023-05-25 18:41:47 -0500

    se moifico

---
+++ tren.py
@@ -0,0 +1,48 @@
+from tkinter import *
+
+#...................
+# variables globales
+#...................
+x = 560
+y = 560
+
+#..................
+# ventana principal
+#..................
+ventana_principal = Tk()
+ventana_principal.title("Graficas 2D")
+ventana_principal.geometry("600x600")
+ventana_principal.config(bg="white")
+
+#.....................
+# frame de graficacion
+#.....................
+frame_graficacion= Frame(ventana_principal)
+frame_graficacion.config(bg="pink",width=580, height=580)
+frame_graficacion.place(x=10,y=10)
+
+# creacion canvas
+c = Canvas(frame_graficacion,width=x, height=y)
+c.config(bg="black")
+c.place(x=10,y=10)
+
+# dibujo del tren
+
+paso_1 = c.create_rectangle(x/2-35,y/2-30,x/2+30,y/2+30, fill= "MediumOrchid4")
+paso_2 = c.create_rectangle(x/2-25,y/2-18,x/2+20,y/2+18, fill= "MediumOrchid2")
+paso_3 = c.create_rectangle(x/2-50,y/2-40,x/2+45,y/2-20, fill= "PaleGreen1")
+paso_4 = c.create_rectangle(x/2-45,y/2-50,x/2+40,y/2-40, fill= "SeaGreen3")
+paso_5 = c.create_rectangle(x/2-150,y/2+30,x/2+40,y/2+120, fill= "SeaGreen4")
+paso_6=c.create_rectangle(x/2-130,y/2-10,x/2-100, y/2+30, fill="red")
+
+
+paso_7=c.create_rectangle(x/2-130,y/2-10,x/2-100, y/2+30, fill="blue")
+
+
+paso_g1 = c.create_oval(x/2-25,y/2+100,x/2+25,y/2+150, fill= "blue")
+paso_g2 = c.create_oval(x/2-85,y/2+100,x/2-35,y/2+150, fill= "blue")
+paso_g3 = c.create_oval(x/2-145,y/2+100,x/2-95,y/2+150, fill= "blue")
+paso_chiq = c.create_oval(x/2-25,y/2-18,x/2+20,y/2+18, fill= "white")
+
+
+ventana_principal.mainloop()
